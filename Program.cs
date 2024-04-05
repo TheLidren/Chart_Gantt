3195a5339e565ad4aa81fcf1aae027cad5f741de
Vladislav <vlad.misevich.2003@mail.ru>
2024-04-05 21:00:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System.Reflection;
+namespace Report_Lotus
+{
+    internal class Program
+    {
+        private static Application oXL;
+        private static _Workbook oWB;
+        private static _Worksheet oSheet;
+        private static Microsoft.Office.Interop.Excel.Range oRng;
+        static void Main(string[] args)
+        {
+            oXL = new Application();
+            oXL.Visible = true;
+
+            oWB = (_Workbook)(oXL.Workbooks.Add(Missing.Value));
+            oSheet = (_Worksheet)oWB.ActiveSheet;
+
+            oSheet.Cells[1, 1] = "First Name";
+            oSheet.Cells[1, 2] = "Last Name";
+            oSheet.Cells[1, 3] = "Full Name";
+            oSheet.Cells[1, 4] = "Salary";
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
